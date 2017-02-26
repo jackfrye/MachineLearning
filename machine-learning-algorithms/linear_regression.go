c07c7d928361903f43a89eb061c1c9d31740107f
John Frye <John Frye>
2017-02-26 00:24:40 -0500

    Calculate total delta from normal equation hypothesis

--- machine-learning-algorithms/linear_regression.go
+++ machine-learning-algorithms/linear_regression.go
@@ -1,11 +1,13 @@
 package mlearn
 
 import (
-  "github.com/gonum/matrix/mat64"
+	"github.com/gonum/matrix/mat64"
 )
 
+//NormalEquation performs the normal equation on a design matrix and
+//solution
 func NormalEquation(X *mat64.Dense, y *mat64.Dense) mat64.Dense {
-  var XT = X.T();
+	var XT = X.T()
 
 	var XTX mat64.Dense
 	XTX.Mul(XT, X)
@@ -14,10 +16,33 @@ func NormalEquation(X *mat64.Dense, y *mat64.Dense) mat64.Dense {
 	_ = XTXInverse.Inverse(&XTX)
 
 	var XTransposeY mat64.Dense
-	XTransposeY.Mul(XT, y);
+	XTransposeY.Mul(XT, y)
 
 	var theta mat64.Dense
-	theta.Mul(&XTXInverse, &XTransposeY);
+	theta.Mul(&XTXInverse, &XTransposeY)
 
-  return theta
+	return theta
+}
+
+//CalculateTotalDelta takes the design matrix, the solution vector and the
+//hypothesis and calculates the total delta
+func CalculateTotalDelta(X *mat64.Dense, theta *mat64.Dense, y *mat64.Dense) float64 {
+	rows, cols := X.Dims()
+
+	delta := 0.0
+
+	for i := 0; i < rows-1; i++ {
+		trainingExample := mat64.NewDense(1, cols, X.RawRowView(i))
+
+		var expectedValueMat mat64.Dense
+		expectedValueMat.Mul(trainingExample, theta)
+		expectedValue := expectedValueMat.RawRowView(0)[0]
+
+		thisY := y.RawRowView(i)[0]
+
+		thisDelta := (expectedValue - thisY) * (expectedValue - thisY)
+		delta += thisDelta
+	}
+
+	return delta
 }
